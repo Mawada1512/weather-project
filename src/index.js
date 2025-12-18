7b08a9ae8b224befae144f254e1b5a9f42b2ebb7
Mawada1512811 <mawadabeedy5@gmail.com>
2025-12-19 01:56:59 +0200

    end fetching and  refreshing data

--- src/index.js
+++ src/index.js
@@ -9,10 +9,45 @@ function searchCity(cityValue) {
   axios.get(apiUrl).then(engineData);
 }
 function engineData(response) {
-  let temp = document.querySelector("#temp");
+  let temperature = document.querySelector("#temp");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
   let city = document.querySelector("#current-city");
   city.innerHTML = response.data.city;
-  temp.innerHTML = Math.round(response.data.temperature.current);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humedity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  time.innerHTML = formatDate(date);
+  let icon = document.querySelector("#current-degree-icon");
+  icon.innerHTML = `<img
+      src="${response.data.condition.icon_url}"
+      class="current-degree-icon"
+    />`;
+}
+function formatDate(dota) {
+  let minutes = dota.getMinutes();
+  let hours = dota.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuseday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dota.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 let searchForm = document.querySelector("#search-form");
